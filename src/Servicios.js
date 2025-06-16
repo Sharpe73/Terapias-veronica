0374c03377aaa6902f6617d30610321dbc9b3256
Héctor Sharpe <cobranzaarriendos@gmail.com>
2025-06-16 14:12:10 -0400

    mejora mensaje

--- src/Servicios.js
+++ src/Servicios.js
@@ -84,9 +84,9 @@ function Servicios() {
                 </p>
               </div>
               <a
-                href={`https://wa.me/56979854465?text=Hola Verónica, quisiera más información sobre el ${encodeURIComponent(
+                href={`https://wa.me/56979854465?text=Hola Verónica, quisiera más información sobre el *${encodeURIComponent(
                   servicio.nombre
-                )}`}
+                )}*`}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center justify-center bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded-full shadow transition"
